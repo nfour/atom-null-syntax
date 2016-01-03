5ba75378aff0e7a345bfda388c4ed6b5cd02e06c
Sam J <novus.nfour@gmail.com>
2016-01-03 18:18:13 +1000

    Init

---
+++ tests/js.js
@@ -0,0 +1,41 @@
+// Babel
+
+class Test extends ( require('test').Prop().prop ) {
+	async constructor() {
+		this.test(function() {
+			var wew = lad;
+		});
+
+		if ( a ) {
+
+		}
+		let a = 1
+		const b = false
+
+		await test();
+		someShit();
+		oy.vey(fn());
+
+		new Promise((resolve, reject) => {
+			resolve();
+		})
+		parseInt()
+		this.parseInt();
+
+		/woooLad/.test()
+
+		return <div className="feed-list">
+	      <h3>News Feed's</h3>
+	      <ul>
+	        {feeds.map(function(feed) {
+	          return <li key={feed.name} className={feed.fetched ? 'loaded' : 'loading'}>
+	            {feed.data && feed.data.length > 0 ?
+	              <span>{feed.name} <span className='light'>({feed.data.length})</span></span>
+	              : 'feed.name' }
+	          </li>
+	        })}
+	      </ul>
+	    </div>;
+	  }
+	}
+};
