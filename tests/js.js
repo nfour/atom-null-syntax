fb06a0deb60901cca0aaa1fe4f0d6a72283b81bc
nfour <novus.nfour@gmail.com>
2016-03-01 17:04:00 +1000

    change builtin variables

--- tests/js.js
+++ tests/js.js
@@ -4,46 +4,66 @@ var Something = require('test');
 var util      = require('util');
 
 class Test extends ( require('test').Prop().prop ) {
-	async constructor() {
-		this.test(function() {
-			var wew = lad;
-		});
+    async constructor() {
+        this.test(function() {
+            var wew = lad;
+        });
 
-		let highlighted = 1
+        let highlighted = 1
 
-		highlighted || highlighted
+        highlighted || highlighted
 
-		let a = 1
-		if ( a ) { }
+        let a = 1
+        if ( a ) { }
 
-		const SOME_CONSTANT = false
+        const SOME_CONSTANT = false
 
-		oy.vey(shoah());
+        oy.vey(shoah());
 
-		await new this.is.some['aa'].Thing()
+        await new this.is.some['aa'].Thing()
 
-		new Promise((resolve, reject) => {
-			resolve();
-		})
-		new Prromise((resolve, reject) => resolve())
-		parseInt()
-		this.parseInt();
+        new Promise((resolve, reject) => {
+            resolve();
+        })
+        new Prromise((resolve, reject) => resolve())
+        parseInt()
+        this.parseInt();
 
-		/woooLad/.test()
+        /woooLad/.test()
 
-		return (
-			<div className="feed-list">
-				<h3>News Feed's</h3>
-				<ul>
-					{feeds.map(function(feed) {
-						return <li key={feed.name} className={feed.fetched ? 'loaded' : 'loading'}>
-							{feed.data && feed.data.length > 0 ?
-							<span>{feed.name} <span className='light'>({feed.data.length})</span></span>
-							: 'feed.name' }
-						</li>
-					})}
-				</ul>
-		    </div>
-		)
-	}
+        switch ( 1) {
+            case "weee":
+            case test:
+        }
+
+        const fn = (
+            a = 1
+        ) => { return 1 }
+
+        process.argv.NODE_ENV
+        module.exports.test
+        module
+        exports
+        __dirname
+        __filename
+
+        const fn = (a = this.a()) => {
+
+        }
+
+        return (
+            <div className="feed-list">
+                <h3>News Feed's</h3>
+                <ul>
+                    {feeds.map(function(feed) {
+                        return <li key={feed.name} className={feed.fetched ? 'loaded' : 'loading'}>
+                            {feed.data && feed.data.length > 0 ?
+                            <span>{feed.name} <span className='light'>({feed.data.length})</span></span>
+                            : 'feed.name' }
+                        </li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
 };
